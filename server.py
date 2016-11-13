3ce58b2b3800febd33b1053c92bff6928ed8e823
Sanil Shah <ss4924@columbia.edu>
2016-11-12 22:31:35 -0500

    handled no review menu items

--- server.py
+++ server.py
@@ -111,15 +111,18 @@ def menu():
     return redirect('/')
   rname = cursor.fetchone()['restaurant_name']
   cursor.close()
-  cursor = g.conn.execute("SELECT m.menu_item_id, m.menu_name, avg(r.rating) as avg, count(r.rating) as cnt FROM menu_items as m, served_at as s, reviews as r, rate as q where m.menu_item_id = s.menu_item_id and s.restaurant_id=%s and q.menu_item_id=m.menu_item_id and q.review_id=r.review_id group by m.menu_name, m.menu_item_id order by avg DESC", rid)
-  if cursor.rowcount == 0:
-    cursor.close()
-    return redirect('/noresults')
+  cursor = g.conn.execute("SELECT m.menu_item_id, m.menu_name, c.cuisine_name, avg(r.rating) as avg, count(r.rating) as cnt FROM menu_items as m, served_at as s, reviews as r, rate as q, cuisines as c, belongs_to as b where b.menu_item_id=m.menu_item_id and c.cuisine_name=b.cuisine_name and m.menu_item_id = s.menu_item_id and s.restaurant_id=%s and q.menu_item_id=m.menu_item_id and q.review_id=r.review_id group by m.menu_name, m.menu_item_id, c.cuisine_name order by avg DESC", rid)
   names = []
   for result in cursor:
     avg = '{0:.2f}'.format(result['avg']) + ' / 10'
-    names.append((result['menu_item_id'], result['menu_name'], avg, result['cnt']))  
+    names.append((result['menu_item_id'], result['menu_name'], result['cuisine_name'], avg, result['cnt']))
   cursor.close()
+  cursor = g.conn.execute("SELECT m.menu_item_id, m.menu_name, c.cuisine_name FROM menu_items as m, served_at as s, rate as q, cuisines as c, belongs_to as b where b.menu_item_id=m.menu_item_id and c.cuisine_name=b.cuisine_name and m.menu_item_id=s.menu_item_id and s.restaurant_id=%s and not exists (select * from rate where rate.menu_item_id=m.menu_item_id) group by m.menu_item_id, m.menu_name, c.cuisine_name order by c.cuisine_name", rid)
+  for result in cursor:
+    names.append((result['menu_item_id'], result['menu_name'], result['cuisine_name'], 'n/a', 0))
+  cursor.close()
+  if not len(names):
+    return redirect('/noresults')
   context = dict(data = names, rname = rname)
   return render_template('menu.html', **context)
 
