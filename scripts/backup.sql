4d027e991f9809eb356863f5793d2350f9c8c3e1
Jason Zhao <zhao.s.jason@gmail.com>
2016-11-13 01:37:41 -0500

    Added input generation scripts

---
+++ scripts/backup.sql
@@ -0,0 +1,493 @@
+insert into restaurants values 
+(16, 'Black Tap'),
+(17, 'Shake Shack'),
+(18, 'Five Guys'),
+(19, 'Mels'),
+(20, 'Umami Burger'),
+(21, 'Momofuku'),
+(22, 'Salvation Burger'),
+(23, 'Harlem Public'),
+(24, 'Wendys');
+
+
+insert into address values 
+(16, '450 W 18th St.', 'New York', 'NY', 'USA', '10036'),
+(17, '218 Broadway', 'New York', 'NY', 'USA', '10027'),
+(18, '442 Broadway', 'New York', 'NY', 'USA', '10027'),
+(19, '1600 Amsterdam Ave.', 'New York', 'NY', 'USA', '10027'),
+(20, '620 E 14th St.', 'New York', 'NY', 'USA', '10036'),
+(21, '8480 8th Ave.', 'New York', 'NY', 'USA', '10036'),
+(22, '772 E 49th St.', 'New York', 'NY', 'USA', '10036'),
+(23, '847 W 126th St.', 'New York', 'NY', 'USA', '10027'),
+(24, '533 W 112th St.', 'New York', 'NY', 'USA', '10027');
+
+insert into located_at values
+(16, 16),
+(17, 17),
+(18, 18),
+(19, 19),
+(20, 20),
+(21, 21),
+(22, 22),
+(23, 23),
+(24, 24);
+
+
+
+
+
+
+
+insert into menu_items values 
+(29, 'The All-American'),
+(30, 'The Californian'),
+(31, 'The Texan'),
+(32, 'The Steak Au Poivre'),
+(33, 'The Bison Burger'),
+(34, 'The Spicy Mexican'),
+(35, 'The Old Fashioned'),
+(36, 'The Greg Norman');
+(37, 'Idaho Fries'),
+(38, 'Oreo Cookies & Cream'),
+(39, 'Peanut Butter'),
+(40, 'Vanilla'),
+(41, 'Coffee'),
+(42, 'Strawberry'),
+(43, 'Nutella'),
+(44, 'You Bet Chocolate');
+
+insert into served_at values
+(29, 16),
+(30, 16),
+(31, 16),
+(32, 16),
+(33, 16),
+(34, 16),
+(35, 16),
+(36, 16),
+(37, 16),
+(38, 16),
+(39, 16),
+(40, 16),
+(41, 16),
+(42, 16),
+(43, 16),
+(44, 16);
+
+insert into belongs_to values
+(29, 'Burger'),
+(30, 'Burger'),
+(31, 'Burger'),
+(32, 'Burger'),
+(33, 'Burger'),
+(34, 'Burger'),
+(35, 'Burger'),
+(36, 'Burger'),
+(37, 'Fries'),
+(38, 'Shakes'),
+(39, 'Shakes'),
+(40, 'Shakes'),
+(41, 'Shakes'),
+(42, 'Shakes'),
+(43, 'Shakes'),
+(44, 'Shakes');
+
+insert into menu_items values
+(45, 'ShackBurger'),
+(46, 'SmokeShack'),
+(47, 'Hamburger'),
+(48, 'Shroom Burger'),
+(49, 'Shack Stack'),
+(50, 'Chickn Shack'),
+(51, 'Fries'),
+(52, 'Cheese Fries'),
+(53, 'Shack Attack'),
+(54, 'Wall-nut Street'),
+(55, 'Jellys Last Donut'),
+(56, 'Brooklyn Pie Oh My'),
+(57, 'Banana Post Rd.'),
+(58, 'Vanilla'),
+(59, 'Chocolate'),
+(60, 'Black & White'),
+(61, 'Strawberry'),
+(62, 'Peanut Butter'),
+(63, 'Coffee');
+
+insert into served_at values
+(45, 17),
+(46, 17),
+(47, 17),
+(48, 17),
+(49, 17),
+(50, 17),
+(51, 17),
+(52, 17),
+(53, 17),
+(54, 17),
+(55, 17),
+(56, 17),
+(57, 17),
+(58, 17),
+(59, 17),
+(60, 17),
+(61, 17),
+(62, 17),
+(63, 17);
+
+insert into belongs_to values
+(45, 'Burger'),
+(46, 'Burger'),
+(47, 'Burger'),
+(48, 'Burger'),
+(49, 'Burger'),
+(50, 'Fried Chicken'),
+(51, 'Fries'),
+(52, 'Fries'),
+(53, 'Ice Cream'),
+(54, 'Ice Cream'),
+(55, 'Ice Cream'),
+(56, 'Ice Cream'),
+(57, 'Ice Cream'),
+(58, 'Shakes'),
+(59, 'Shakes'),
+(60, 'Shakes'),
+(61, 'Shakes'),
+(62, 'Shakes'),
+(63, 'Shakes');
+
+
+insert into menu_items values
+(64, 'Hamburger'),
+(65, 'Cheeseburger'),
+(66, 'Bacon Burger'),
+(67, 'Bacon Cheeseburger'),
+(68, 'Veggie Sandwich'),
+(69, 'Cheese Veggie Sandwich'),
+(70, 'Five Guys Style Fries'),
+(71, 'Cajun Style Fries'),
+(72, 'Bacon'),
+(73, 'Banana'),
+(74, 'Chocolate'),
+(75, 'Coffee'),
+(76, 'Oreo Cookie'),
+(77, 'Peanut Butter'),
+(78, 'Strawberry');
+
+insert into served_at values
+(64, 18),
+(65, 18),
+(66, 18),
+(67, 18),
+(68, 18),
+(69, 18),
+(70, 18),
+(71, 18),
+(72, 18),
+(73, 18),
+(74, 18),
+(75, 18),
+(76, 18),
+(77, 18),
+(78, 18);
+
+insert into belongs_to values
+(64, 'Burger'),
+(65, 'Burger'),
+(66, 'Burger'),
+(67, 'Burger'),
+(68, 'Burger'),
+(69, 'Burger'),
+(70, 'Fries'),
+(71, 'Fries'),
+(72, 'Shakes'),
+(73, 'Shakes'),
+(74, 'Shakes'),
+(75, 'Shakes'),
+(76, 'Shakes'),
+(77, 'Shakes'),
+(78, 'Shakes');
+
+insert into menu_items values
+(79, 'The Classic'),
+(80, 'Cheeseburger Baby'),
+(81, 'Cadillac Burger'),
+(82, 'Nurse Betty'),
+(83, 'Back Yarder'),
+(84, 'Mels Double Double'),
+(85, 'California Burger'),
+(86, 'Bacon Mishroom Melt'),
+(87, 'Hot Mess'),
+(88, 'WTF'),
+(89, 'French Onion'),
+(90, 'Sliders with Cheese'),
+(91, 'Widowmaker'),
+(92, 'The Cowboy'),
+(93, 'Dirty Hipster'),
+(94, 'Bo Burger'),
+(95, 'Fat Mexican'),
+(96, 'The Broadway'),
+(97, 'Fresh Cut'),
+(98, 'Atomic'),
+(99, 'Garlic'),
+(100, 'Sweet Potato'),
+(101, 'Parmesan Jalapeno'),
+(102, 'Chili Cheese'),
+(103, 'Disco'),
+(104, 'Bacon, Sour Cream, and Chives'),
+(105, 'Chicken Fingers');
+
+insert into served_at values
+(79, 19),
+(80, 19),
+(81, 19),
+(82, 19),
+(83, 19),
+(84, 19),
+(85, 19),
+(86, 19),
+(87, 19),
+(88, 19),
+(89, 19),
+(90, 19),
+(91, 19),
+(92, 19),
+(93, 19),
+(94, 19),
+(95, 19),
+(96, 19),
+(97, 19),
+(98, 19),
+(99, 19),
+(100, 19),
+(101, 19),
+(102, 19),
+(103, 19),
+(104, 19),
+(105, 19);
+
+insert into belongs_to values
+(79, 'Burger'),
+(80, 'Burger'),
+(81, 'Burger'),
+(82, 'Burger'),
+(83, 'Burger'),
+(84, 'Burger'),
+(85, 'Burger'),
+(86, 'Burger'),
+(87, 'Burger'),
+(88, 'Burger'),
+(89, 'Burger'),
+(90, 'Burger'),
+(91, 'Burger'),
+(92, 'Burger'),
+(93, 'Burger'),
+(94, 'Burger'),
+(95, 'Burger'),
+(96, 'Burger'),
+(97, 'Fries'),
+(98, 'Fries'),
+(99, 'Fries'),
+(100, 'Fries'),
+(101, 'Fries'),
+(102, 'Fries'),
+(103, 'Fries'),
+(104, 'Fries'),
+(105, 'Fried Chicken');
+
+
+insert into menu_items values
+(106, 'Sunny Side'),
+(107, 'Greenbird'),
+(108, 'Manly'),
+(109, 'Throwback'),
+(110, 'Umami'),
+(111, 'Truffle'),
+(112, 'Cali'),
+(113, 'K-BBQ'),
+(114, 'Hatch'),
+(115, 'Ahi Tuna'),
+(116, 'Thin'),
+(117, 'Truffle'),
+(118, 'Sweet Potato'),
+(119, 'Maple Bacon');
+
+insert into served_at values
+(106, 20),
+(107, 20),
+(108, 20),
+(109, 20),
+(110, 20),
+(111, 20),
+(112, 20),
+(113, 20),
+(114, 20),
+(115, 20),
+(116, 20),
+(117, 20),
+(118, 20),
+(119, 20);
+
+insert into belongs_to values
+(106, 'Burger'),
+(107, 'Burger'),
+(108, 'Burger'),
+(109, 'Burger'),
+(110, 'Burger'),
+(111, 'Burger'),
+(112, 'Burger'),
+(113, 'Burger'),
+(114, 'Burger'),
+(115, 'Burger'),
+(116, 'Fries'),
+(117, 'Fries'),
+(118, 'Fries'),
+(119, 'Fries');
+
+
+
+insert into menu_items values
+(120, 'Impossible Burger');
+
+insert into served_at values
+(120, 21);
+
+insert into belongs_to values
+(120, 'Burger');
+
+
+
+insert into menu_items values
+(121, 'Salvation Burger'),
+(122, 'Classic Burger'),
+(123, 'Fish on Bun'),
+(124, 'Veggie Burger'),
+(125, 'Ants on a Log'),
+(126, 'Toasted Marshmallow'),
+(127, 'Oat, Walnut, Whiskey (Boozy)'),
+(128, 'Grasshopper Mint (Boozy)'),
+(129, 'Poutine'),
+(130, 'Fries');
+
+insert into served_at values
+(121, 22),
+(122, 22),
+(123, 22),
+(124, 22),
+(125, 22),
+(126, 22),
+(127, 22),
+(128, 22),
+(129, 22),
+(130, 22);
+
+insert into belongs_to values
+(121, 'Burger'),
+(122, 'Burger'),
+(123, 'Burger'),
+(124, 'Burger'),
+(125, 'Shakes'),
+(126, 'Shakes'),
+(127, 'Shakes'),
+(128, 'Shakes'),
+(129, 'Fries'),
+(130, 'Fries');
+
+
+
+insert into menu_items values
+(131, 'Avocado Fries'),
+(132, 'Fries'),
+(133, 'Tater Tots'),
+(134, 'The Public Burger'),
+(135, 'Cheese'),
+(136, 'Good Stuff'),
+(137, 'Peanut Butter Burger'),
+(138, 'BBQ Pork Burger'),
+(139, 'Parmesan Crusted Burger'),
+(140, 'Oh Snap Burger'),
+(141, 'Veggie Burger');
+
+insert into served_at values
+(131, 23),
+(132, 23),
+(133, 23),
+(134, 23),
+(135, 23),
+(136, 23),
+(137, 23),
+(138, 23),
+(139, 23),
+(140, 23),
+(141, 23);
+
+insert into belongs_to values
+(131, 'Fries'),
+(132, 'Fries'),
+(133, 'Fries'),
+(134, 'Burger'),
+(135, 'Burger'),
+(136, 'Burger'),
+(137, 'Burger'),
+(138, 'Burger'),
+(139, 'Burger'),
+(140, 'Burger'),
+(141, 'Burger');
+
+
+insert into menu_items values
+(142, 'Daves Single'),
+(143, 'Daves Double'),
+(144, 'Daves Triple'),
+(145, 'Baconator'),
+(146, 'Son of Baconator'),
+(147, 'Jr. Bacon Cheeseburger'),
+(148, 'Jr. Cheeseburger Deluxe'),
+(149, 'Spicy Chicken Sandwich'),
+(150, 'Homestyle Chicken Sandwich'),
+(151, 'Asiago Ranch Chicken Club'),
+(152, 'Classic Frosty'),
+(153, 'Vanilla Frosty');
+
+insert into served_at values
+(142, 24),
+(143, 24),
+(144, 24),
+(145, 24),
+(146, 24),
+(147, 24),
+(148, 24),
+(149, 24),
+(150, 24),
+(151, 24),
+(152, 24),
+(153, 24);
+
+insert into belongs_to values
+(142, 'Burger'),
+(143, 'Burger'),
+(144, 'Burger'),
+(145, 'Burger'),
+(146, 'Burger'),
+(147, 'Burger'),
+(148, 'Burger'),
+(149, 'Fried Chicken'),
+(150, 'Fried Chicken'),
+(151, 'Fried Chicken'),
+(152, 'Shakes'),
+(153, 'Shakes');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
